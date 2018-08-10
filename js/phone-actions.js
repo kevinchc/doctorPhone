ce98ad2a82983bda02c44a3a5b23c91cc8ce78b9
@@4787chagua <chaguakevin@gmail.com>
2018-08-10 17:49:24 -0500

    actualizado

--- js/phone-actions.js
+++ js/phone-actions.js
@@ -46,57 +46,6 @@ function image() {
 /** End **/
 
 /** Clock  and Battery**/
-Reloj();
-
-function Reloj() {
-    var tiempo = new Date();
-    var hora = tiempo.getHours();
-    var minuto = tiempo.getMinutes();
-    var segundo = tiempo.getSeconds();
-    document.getElementById('hora').innerHTML = hora;
-    document.getElementById('minuto').innerHTML = minuto;
-    document.getElementById('segundo').innerHTML = segundo;
-    setTimeout('Reloj()', 1000);
-    str_hora = new String(hora);
-    if (str_hora.length == 1) {
-        document.getElementById('hora').innerHTML = '0' + hora;
-    }
-    str_minuto = new String(minuto);
-    if (str_minuto.length == 1) {
-        document.getElementById('minuto').innerHTML = ':0' + minuto;
-    } else {
-        document.getElementById('minuto').innerHTML = ':' + minuto;
-    }
-    str_segundo = new String(segundo);
-    if (str_segundo.length == 1) {
-        document.getElementById('segundo').innerHTML = ':0' + segundo;
-    } else {
-        document.getElementById('segundo').innerHTML = ':' + segundo;
-    }
-
-    if (hora >= 8 && hora < 10) {
-        $("#power_battery").text("100%");
-        $('#battery').attr('src', "images/battery1.png");
-    } else if (hora < 12 && hora >= 10) {
-        $("#power_battery").text("80%");
-        $('#battery').attr('src', "images/battery1.png");
-    } else if (hora < 14 && hora >= 12) {
-        $("#power_battery").text("60%");
-        $('#battery').attr('src', "images/battery1.png");
-    } else if (hora < 16 && hora >= 14) {
-        $("#power_battery").text("40%");
-        $('#battery').attr('src', "images/battery2.png");
-    } else if (hora < 18 && hora >= 16) {
-        $("#power_battery").text("20%");
-        $('#battery').attr('src', "images/battery2.png");
-    } else if (hora < 20 && hora >= 18) {
-        $("#power_battery").text("0%");
-        $('#battery').attr('src', "images/battery3.png");
-    } else {
-        $("#power_battery").text("0%");
-        $('#battery').attr('src', "images/battery3.png");
-    }
-}
 
 
 $(window).scroll(function () {
